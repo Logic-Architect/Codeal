b3591b79473f9de1c903ececa5262aa2798f1692
namanTiwari <er.amantiwari.2002@gmail.com>
2023-04-02 23:38:23 +0530

    Creating directories and setting up Express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { clearScreenDown } = require('readline');
+const app = express();
+const port = 8000;
+
+app.listen(port, err=>{
+    if(err){
+        console.log(`Error in running the server ${err}`);
+        return;
+    }
+    console.log(`Server started successfully at port ${port}`);
+})
