17dfcf21d5fe8afb3d7197d43a7911b99e35a725
namanTiwari <er.amantiwari.2002@gmail.com>
2023-04-03 08:37:17 +0530

    Added a controller and a router for Home

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const { clearScreenDown } = require('readline');
 const app = express();
 const port = 8000;
 
+app.use('/',require('./routes/index'));
+
+
 app.listen(port, err=>{
     if(err){
         console.log(`Error in running the server ${err}`);
